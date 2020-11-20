a5573d4596b5060b0627e0c8f3b7e64380397774
Bottrap <74072843+Bottrap@users.noreply.github.com>
2020-11-20 20:07:42 +0100

    diocane

--- app/src/main/java/com/example/mystore_1_0/MainActivity.java
+++ app/src/main/java/com/example/mystore_1_0/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //elimina barra di stato dal layout del cell
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
