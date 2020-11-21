05f6bee03b4d2ea4dda19ec38aa631cb83bc8ad9
KINERYH <pietrovp@live.it>
2020-11-21 14:07:46 +0100

    Aggiornamento

--- app/src/main/java/com/example/mystore_1_0/ScanActivity.java
+++ app/src/main/java/com/example/mystore_1_0/ScanActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -17,5 +18,9 @@ public class ScanActivity extends AppCompatActivity {
     public void clickAdminBtn(View v){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void clickScanBtn(View v){
+        Toast.makeText(this, "Non hai la fotocamera, coglione!", Toast.LENGTH_SHORT).show();
     }
 }
