deca7bd9dcff547a8d0f78c1c437a2e1f81c860f
KINERYH <pietrovp@live.it>
2020-11-21 20:41:24 +0100

    Aggiornamento

--- app/src/main/java/com/example/mystore_1_0/ScanActivity.java
+++ app/src/main/java/com/example/mystore_1_0/ScanActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -13,9 +14,16 @@ public class ScanActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //elimina barra di stato dal layout del cell
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_scan);
+
+        Window window = this.getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // aggiunge il flag FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS alla window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        // finalmente cambia il colore
+        window.setStatusBarColor(this.getResources().getColor(R.color.scanStatusBarColor));
+
     }
 
     public void clickAdminBtn(View v){
