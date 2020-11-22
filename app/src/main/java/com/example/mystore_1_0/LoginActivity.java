e510209551d605813b8cc5a8f75129f69a63cc0b
Bottrap <74072843+Bottrap@users.noreply.github.com>
2020-11-22 18:02:21 +0100

    database agg

--- app/src/main/java/com/example/mystore_1_0/LoginActivity.java
+++ app/src/main/java/com/example/mystore_1_0/LoginActivity.java
@@ -6,18 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-    String nome, password;
+    EditText editId, editPassw;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editId = findViewById(R.id.editId);
+        editPassw = findViewById(R.id.editPassw);
+
     }
 
-    public void clickLogin(View view) {
+    public void clickLogin(View view){
 
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("Users");
+
+        String id = editId.getText().toString();
+        String password = editPassw.getText().toString();
+
+        UserData data = new UserData(id, password);
+        reference.setValue(data);
+
+        Toast.makeText(this, "registrazione effettuata", Toast.LENGTH_SHORT).show();
     }
 }
