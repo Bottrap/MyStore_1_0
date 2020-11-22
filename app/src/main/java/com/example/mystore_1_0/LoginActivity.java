216b349da4d573ec9d5179237842de5318432b91
Bottrap <74072843+Bottrap@users.noreply.github.com>
2020-11-22 14:22:22 +0100

    agg

--- app/src/main/java/com/example/mystore_1_0/LoginActivity.java
+++ app/src/main/java/com/example/mystore_1_0/LoginActivity.java
@@ -3,14 +3,21 @@ package com.example.mystore_1_0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String nome, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void clickLogin(View view) {
+
     }
 }
