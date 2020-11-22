646eb6ee44bd3d758dc1b5442a15cd650b710c97
KINERYH <pietrovp@live.it>
2020-11-22 19:45:05 +0100

    Aggiornamento

--- app/src/main/java/com/example/mystore_1_0/LoginActivity.java
+++ app/src/main/java/com/example/mystore_1_0/LoginActivity.java
@@ -1,16 +1,21 @@
 package com.example.mystore_1_0;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,12 +39,69 @@ public class LoginActivity extends AppCompatActivity {
         rootNode = FirebaseDatabase.getInstance();
         reference = rootNode.getReference("Users");
 
-        String id = editId.getText().toString();
-        String password = editPassw.getText().toString();
+        String id = editId.getText().toString().trim();
+        String password = editPassw.getText().toString().trim();
 
-        UserData data = new UserData(id, password);
+        Utente data = new Utente(id, password);
         reference.setValue(data);
 
-        Toast.makeText(this, "registrazione effettuata", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Registrazione effettuata", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickBtnProva(View view) {
+
+        String id = editId.getText().toString().trim();
+        String password = editPassw.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        Query checkId = reference.orderByChild("id").equalTo(id);
+        checkId.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String passwFromDB = dataSnapshot.child(id).child("password").getValue().toString();
+                    if (passwFromDB.equals(password)){
+                        //annullo gli errori su id e password (penso solo in material design)
+
+                        String idFromDB = dataSnapshot.child(id).child("id").getValue().toString();
+                        String permessiFromDB = dataSnapshot.child(id).child("permessi").getValue().toString();
+                        String nomeFromDB = dataSnapshot.child(id).child("nome").getValue().toString();
+                        String cognomeFromDB = dataSnapshot.child(id).child("cognome").getValue().toString();
+                        String dataNascitaFromDB = dataSnapshot.child(id).child("dataNascita").getValue().toString();
+                        String telefonoFromDB = dataSnapshot.child(id).child("telefono").getValue().toString();
+                        Utente utente = new Utente(idFromDB, password, permessiFromDB, nomeFromDB, cognomeFromDB, dataNascitaFromDB, telefonoFromDB);
+
+                        /*Intent intent = new Intent(this, nuovaActivity.class);
+
+                        intent.putExtra("utente", utente);
+
+                        intent.putExtra("id", idFromDB);
+                        intent.putExtra("permessi", permessiFromDB);
+                        intent.putExtra("nome", nomeFromDB);
+                        intent.putExtra("cognome", cognomeFromDB);
+                        intent.putExtra("telefono", telefonoFromDB);
+                        intent.putExtra("dataNascita", dataNascitaFromDB);
+                        intent.putExtra("permessi", permessiFromDB);
+
+                        startActivity(intent);
+                        */
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "Password errata", Toast.LENGTH_SHORT).show();
+                        //password.setError("È stata inserita una password errata");
+                        //password.requestFocus();
+                    }
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Id non trovato", Toast.LENGTH_SHORT).show();
+                    //id.setError("È stato inserito un Id non esistente");
+                    //id.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
     }
 }
