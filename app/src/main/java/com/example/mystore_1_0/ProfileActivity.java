1972a31d0d4740cc23ea5135c3c1261286d1d3d7
Bottrap <74072843+Bottrap@users.noreply.github.com>
2020-11-23 02:20:44 +0100

    drawer part 2 agg

--- app/src/main/java/com/example/mystore_1_0/ProfileActivity.java
+++ app/src/main/java/com/example/mystore_1_0/ProfileActivity.java
@@ -1,16 +1,20 @@
 package com.example.mystore_1_0;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
+import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toolbar;
+
 
 import com.google.android.material.navigation.NavigationView;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -19,6 +23,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
 
        /* TextView testo;
         @Override
@@ -31,10 +36,36 @@ public class ProfileActivity extends AppCompatActivity {
             testo.setText(nomeUtente);
         */
 
+        //aggancia le variabili
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
+        //toolbar
+        setSupportActionBar(toolbar);
 
+        //drawer menu
+        navigationView.bringToFront(); //si evidenzia alla pressione
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
         }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+}
