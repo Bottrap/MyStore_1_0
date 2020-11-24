ad17608f7c74b012cc8e4d25dc9128c6566f2444
Bottrap <74072843+Bottrap@users.noreply.github.com>
2020-11-24 22:56:06 +0100

    fragments adduser

--- app/src/main/java/com/example/mystore_1_0/Fragments/AddUsersFragment.java
+++ app/src/main/java/com/example/mystore_1_0/Fragments/AddUsersFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,13 +15,26 @@ import androidx.fragment.app.Fragment;
 import com.example.mystore_1_0.ProfileActivity;
 import com.example.mystore_1_0.R;
 
+import java.util.ArrayList;
+
 public class AddUsersFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+        View view = inflater.inflate(R.layout.fragment_addusers, container, false);
         Toast.makeText(getActivity(), "Aggiungi Dipendenti", Toast.LENGTH_SHORT).show();
 
-        return inflater.inflate(R.layout.fragment_addusers, container, false);
+       // val items = listOf("Material", "Design", "Components", "Android")
+       // val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+
+        String [] tipopermessi = new String[] {"1","2","3"};
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item, tipopermessi);
+
+        AutoCompleteTextView editTextFilledExposedDropdown = view.findViewById(R.id.text_permessi);
+        editTextFilledExposedDropdown.setAdapter(adapter);
+
+        return view;
     }
 }
