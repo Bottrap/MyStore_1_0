66cdbbaf1ce09ce2889657f0621dfe0c881a6b2f
KINERYH <pietrovp@live.it>
2020-12-15 21:11:45 +0100

    aggiunte card view nella dashboard

--- app/src/main/java/com/example/mystore_1_0/Activity/MapActivity.java
+++ app/src/main/java/com/example/mystore_1_0/Activity/MapActivity.java
@@ -13,7 +13,6 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mappa);
-
         Intent intent = getIntent();
     }
 }
--- app/src/main/java/com/example/mystore_1_0/Fragments/DashboardFragment.java
+++ app/src/main/java/com/example/mystore_1_0/Fragments/DashboardFragment.java
@@ -7,21 +7,54 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.example.mystore_1_0.Fragments.Profile.ProfileFragment;
+import com.example.mystore_1_0.Fragments.ShowUsers.ShowUsersFragment;
 import com.example.mystore_1_0.R;
 
 public class DashboardFragment extends Fragment {
 
-    CardView showUsersCV;
+    CardView addUserCV, showUsersCV, qrScanCV, profileCV, logoutCV;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
+        addUserCV = view.findViewById(R.id.adduser_cardview);
         showUsersCV = view.findViewById(R.id.showusers_cardview);
+        qrScanCV = view.findViewById(R.id.qrGen_cardView);
+        profileCV = view.findViewById(R.id.profile_cardView);
+        logoutCV = view.findViewById(R.id.logout_cardView);
+
+        addUserCV.setOnClickListener(v -> {
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddUsersFragment()).commit();
+        });
+
+        showUsersCV.setOnClickListener(v -> {
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShowUsersFragment()).commit();
+        });
+
+        qrScanCV.setOnClickListener(v -> {
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new QrGeneratorFragment()).commit();
+        });
+
+        profileCV.setOnClickListener(v -> {
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+        });
+
+        logoutCV.setOnClickListener(v -> {
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+            activity.finish();
+        });
+
 
 
         return view;
