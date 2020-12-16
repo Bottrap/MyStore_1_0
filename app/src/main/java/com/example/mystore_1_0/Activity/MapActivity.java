d7b80cf0cfbd1bbb4fcfa73ae766b82f4358d741
Bottrap <74072843+Bottrap@users.noreply.github.com>
2020-12-16 21:45:21 +0100

    bottoni

--- app/src/main/java/com/example/mystore_1_0/Activity/MapActivity.java
+++ app/src/main/java/com/example/mystore_1_0/Activity/MapActivity.java
@@ -3,7 +3,12 @@ package com.example.mystore_1_0.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 import com.example.mystore_1_0.R;
 
@@ -14,6 +19,24 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mappa);
 
-        Intent intent = getIntent();
+
+        //Button button00 = findViewById(R.id.button_prova);
+        GridLayout gridLayout = findViewById(R.id.gridlayout);
+
+        gridLayout.getChildAt(1).setVisibility(View.VISIBLE);
+        gridLayout.getChildAt(1).setBackgroundColor(getResources().getColor(R.color.transparent));
+        gridLayout.getChildAt(23).setVisibility(View.VISIBLE);
+        gridLayout.getChildAt(23).setBackgroundColor(getResources().getColor(R.color.transparent));
+
+
+        //gridLayout.getChildAt(11).setVisibility(View.INVISIBLE);
+        //gridLayout.getChildAt(12).setVisibility(View.INVISIBLE);
+        //gridLayout.getChildAt(10).getLayoutParams();
+        Log.d("X",String.valueOf(gridLayout.getChildAt(10).getLayoutParams()));
+        Log.d("material",String.valueOf(gridLayout.getChildAt(23)));
+        Log.d("count", String.valueOf(gridLayout.getChildCount()));
+
+
+
     }
 }
